8c144ee5e40f324d2e6428d3f141341903328c0f
dheerajstvpm <dheerajstvpm@gmail.com>
2022-10-11 12:39:25 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -10,31 +10,149 @@ const Offer = require('../models/offerModel')
 const Product = require('../models/productModel')
 const Order = require('../models/orderModel')
 
+const config = require('./config')
+const client = require('twilio')(config.accountSid, config.authToken)
+
 const { check, validationResult } = require('express-validator');
 
 let session;
 
+let name1;
+let mobileNumber1;
+let username1;
+let password1;
+let address1;
+
 router.get('/', (req, res) => {
     session = req.session;
-    if (session.userId) {
-        res.render('users/homepage', { title: 'Shop.', id: 'session.userId' })
+    Homepage.find({})
+        .then((result) => {
+            if (session.userId) {
+                res.render('users/homepage', { title: 'Shop.', loginId: true, result })
+            } else {
+                res.render('users/homepage', { title: 'Shop.', result })
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+router.get('/product/:id', (req, res) => {
+    session = req.session;
+    const productId = req.params.id;
+
+    Product.find({ _id: productId })
+        .then((result) => {
+            console.log(result);
+            if (session.userId) {
+                res.render('users/product', { title: 'Shop.', loginId: true, result })
+            } else {
+                res.render('users/product', { title: 'Shop.', result })
+            }
+            // res.render('admin/userHomepageLayout', { result });
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+
+
+//OTP Login
+
+// router.get('/otpLogin', (req, res) => {
+//     res.render('users/otpLogin');
+// })   
+// let mobileNumber;
+// router.get('/otpLogin', (req, res) => {
+//     res.render('users/otpLogin')
+// })
+
+// router.post('/otpLogin', (req, res) => {
+//     mobileNumber = req.body.mobileNumber;
+//     if (req.body.mobileNumber) {
+//         client
+//             .verify
+//             .services(config.serviceID)
+//             .verifications
+//             .create({
+//                 to: `+91${req.body.mobileNumber}`,
+//                 channel: 'sms'
+//             })
+//             .then((data) => {
+//                 res.redirect('/otpLoginVerify')
+// res.status(200).send({
+//     message: "Verification is sent!!",
+//     phonenumber: req.body.mobileNumber,
+//     data
+// })
+//             })
+//     } else {
+//         res.status(400).send({
+//             message: "Wrong phone number or code :(",
+//             phonenumber: req.body.mobileNumber
+//         })
+//     }
+
+// })
+
+
+
+router.get('/otpLoginVerify', (req, res) => {
+    res.render('users/otpLoginVerify');
+})
+router.post('/otpLoginVerify', (req, res) => {
+    if ((req.body.otp).length === 6) {
+        client
+            .verify
+            .services(config.serviceID)
+            .verificationChecks
+            .create({
+                to: `+91${mobileNumber1}`,
+                code: req.body.otp
+            })
+            .then((data) => {
+                if (data.status === "approved") {
+                    const user = new User({
+                        name: name1,
+                        mobile: mobileNumber1,
+                        username: username1,
+                        password: password1,
+                        address: address1
+                    })
+                    user.save()
+                        .then((result) => {
+                            console.log('success')
+                        })
+                        .catch((err) => {
+                            console.log(err)
+                        })
+                    res.redirect('/login');
+                    // res.redirect('/')
+                    // res.status(200).send({
+                    //     message: "User is Verified!!",
+                    //     data
+                    // })
+                }
+            })
     } else {
-        res.render('users/homepage', { title: 'Shop.' })
+        res.status(400).send({
+            message: "Wrong phone number or code :(",
+            phonenumber: mobileNumber1
+        })
     }
 })
 
-router.get('/product', (req, res) => {
-    session = req.session;
-    if (session.userId) {
-        res.render('users/product', { title: 'Shop.', id: 'session.userId' })
-    } else {
-        res.render('users/product', { title: 'Shop.' })
-    }
-})
+
+
+
+
+
 
 router.get('/login', (req, res) => {
     session = req.session;
-    if (session.userStatus==='blocked') {
+    if (session.userStatus === 'blocked') {
         console.log(session)
         console.log('5')
         req.session.destroy();
@@ -47,7 +165,7 @@ router.get('/login', (req, res) => {
         console.log('3')
         req.session.destroy();
         res.render('users/login', { title: 'Login', usernameMessage: 'Username does not exist' })
-        
+
     } else if (session.incorrectPwd) {
         console.log(session)
         console.log('4')
@@ -60,14 +178,22 @@ router.get('/login', (req, res) => {
 })
 
 router.get('/signup', (req, res) => {
-    res.render('users/signup', { title: 'Signup' })
+    if (session.userId) {
+        res.redirect('/')
+    } else if (session.userAlreadyExist) {
+        res.render('users/signup', { title: 'Signup', usernameMsg: 'User Already Exists' })
+    } else {
+        res.render('users/signup', { title: 'Signup' })
+    }
 })
 
 router.post('/signup',
     check('name').notEmpty()
         .withMessage('Please enter a Name'),
-    check('mobile').matches(/[\d]{10}/)
+    check('mobileNumber').matches(/[\d]{10}/)
         .withMessage("Mobile number must contain exactly 10 numbers"),
+    check('address').notEmpty()
+        .withMessage('Please enter a Address'),
     check('username').notEmpty()
         .withMessage('Please enter a username'),
     check('username').matches(/^\w+([\._]?\w+)?@\w+(\.\w{2,3})(\.\w{2})?$/)
@@ -85,19 +211,19 @@ router.post('/signup',
     function (req, res) {
         const errors = validationResult(req);
         console.log(errors)
-        const error1 = errors.errors.find(item => item.param === 'name')||'';
-        const error2 = errors.errors.find(item => item.param === 'mobile')||'';
-        const error3 = errors.errors.find(item => item.param === 'username')||'';
-        const error4 = errors.errors.find(item => item.param === 'password')||'';
-        
-        let error5='';
+        const error1 = errors.errors.find(item => item.param === 'name') || '';
+        const error2 = errors.errors.find(item => item.param === 'mobileNumber') || '';
+        const error3 = errors.errors.find(item => item.param === 'username') || '';
+        const error4 = errors.errors.find(item => item.param === 'password') || '';
+        const error6 = errors.errors.find(item => item.param === 'address') || '';
+
+        let error5 = '';
         if (req.body.password != req.body.confirmPassword) {
             error5 = 'Passwords do not match';
         }
         console.log(error5);
         if (!errors.isEmpty()) {
-            res.render('users/signup', { title: 'Signup', nameMsg: error1.msg, mobileMsg: error2.msg, usernameMsg: error3.msg, pwdMsg: error4.msg, confirmPwdMsg: error5 });
-           
+            res.render('users/signup', { title: 'Signup', nameMsg: error1.msg, mobileMsg: error2.msg, usernameMsg: error3.msg, pwdMsg: error4.msg, confirmPwdMsg: error5, addressMsg: error6.msg });
         } else {
             User.find({ username: req.body.username })
                 .then((result) => {
@@ -109,23 +235,44 @@ router.post('/signup',
                             if (user) {
                                 session = req.session;
                                 session.userAlreadyExist = true;
+                                console.log('User Already Exist')
                                 res.redirect('/signup');
-                            } else {
-                                const user = new User({
-                                    name: req.body.name,
-                                    mobile: req.body.mobile,
-                                    username: req.body.username,
-                                    password: hashPassword
-                                })
-                                user.save()
-                                    .then((result) => {
-                                        console.log('success')
-                                    })
-                                    .catch((err) => {
-                                        console.log(err)
-                                    })
-                                res.redirect('/login');
                             }
+                            else {
+                                client
+                                    .verify
+                                    .services(config.serviceID)
+                                    .verifications
+                                    .create({
+                                        to: `+91${req.body.mobileNumber}`,
+                                        channel: 'sms'
+                                    })
+                                    .then((data) => {
+                                        name1 = req.body.name,
+                                            mobileNumber1 = req.body.mobileNumber,
+                                            username1 = req.body.username,
+                                            password1 = hashPassword,
+                                            address1 = req.body.address
+                                        res.redirect('/otpLoginVerify')
+                                    })
+                            }
+                            // else {   
+                            //     const user = new User({
+                            //         name: req.body.name,
+                            //         mobile: req.body.mobile,
+                            //         username: req.body.username,
+                            //         password: hashPassword,
+                            //         address: req.body.address
+                            //     })
+                            //     user.save()
+                            //         .then((result) => {
+                            //             console.log('success')
+                            //         })
+                            //         .catch((err) => {
+                            //             console.log(err)
+                            //         })
+                            //     res.redirect('/login');
+                            // }
                         })
                         .catch((err) => {
                             console.log(err)
@@ -148,7 +295,7 @@ router.post('/login', function (req, res) {
                     if (bcryptResult) {
                         session = req.session;
                         session.userId = temp.username;
-                        session.userStatus=temp.status;
+                        session.userStatus = temp.status;
                         res.redirect('/login');
                     } else {
                         session = req.session
@@ -174,5 +321,17 @@ router.post('/login', function (req, res) {
         })
 });
 
+
+//User logout
+
+router.get('/logout', function (req, res) {
+    session = req.session
+    session.userId = false
+    session.incorrectId = false
+    session.userAlreadyExist = false
+    session.incorrectPwd = false
+    session.userStatus = ""
+    res.redirect('/');
+});
 
 module.exports = router;
