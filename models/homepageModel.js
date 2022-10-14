b5450cbad7db1f047dcb3a5d6803dbfc46806767
dheerajstvpm <dheerajstvpm@gmail.com>
2022-10-14 11:41:50 +0530

    Update homepageModel.js

--- models/homepageModel.js
+++ models/homepageModel.js
@@ -2,34 +2,66 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema;
 
+const productSchema = new Schema({
+    productName: { type: String },
+    description: { type: String },
+    price: { type: String },
+    stock: { type: String },
+    image: { type: String },
+    croppedImage: { type: String },
+    category: { type: String },
+    offer: { type: String }
+}, { timestamps: true })
 
-const homepageSchema=new Schema({
-    firstRow1:{type:String},
-    firstRow1Id:{type:String},
-    firstRow2:{type:String},
-    firstRow2Id:{type:String},
-    firstRow3:{type:String},
-    firstRow3Id:{type:String},
-    firstRow4:{type:String},
-    firstRow4Id:{type:String},
-    banner1:{type:String},
-    banner1Id:{type:String},
-    banner2:{type:String},
-    banner2Id:{type:String},
-    banner3:{type:String},
-    banner3Id:{type:String},
-    lastRow1:{type:String},
-    lastRow1Id:{type:String},
-    lastRow2:{type:String},
-    lastRow2Id:{type:String},
-    lastRow3:{type:String},
-    lastRow3Id:{type:String},
-    lastRow4:{type:String},
-    lastRow4Id:{type:String}
+const homepageSchema = new Schema({
+    firstRow1: {
+        type: productSchema,
+        default: {}
+    },
+    firstRow2: {
+        type: productSchema,
+        default: {}
+    },
+    firstRow3: {
+        type: productSchema,
+        default: {}
+    },
+    firstRow4: {
+        type: productSchema,
+        default: {}
+    },
+    banner1: {
+        type: productSchema,
+        default: {}
+    },
+    banner2: {
+        type: productSchema,
+        default: {}
+    },
+    banner3: {
+        type: productSchema,
+        default: {}
+    },
+    lastRow1: {
+        type: productSchema,
+        default: {}
+    },
+    lastRow2: {
+        type: productSchema,
+        default: {}
+    },
+    lastRow3: {
+        type: productSchema,
+        default: {}
+    },
+    lastRow4: {
+        type: productSchema,
+        default: {}
+    }
 })
 
 
 
 
 
-module.exports =mongoose.model('Homepage', homepageSchema)
+module.exports = mongoose.model('Homepage', homepageSchema)
