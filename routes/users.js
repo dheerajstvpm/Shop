1426b0830cf544a736fad3d1ca046569e6de5000
dheerajstvpm <dheerajstvpm@gmail.com>
2022-10-23 20:08:47 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -39,8 +39,8 @@ router.post('/signup',
         .withMessage("Mobile number must contain exactly 10 numbers"),
     check('mobileNumber').matches(/^[6-9][\d]{9}/)
         .withMessage("Please enter a valid mobile number"),
-    check('address').notEmpty()
-        .withMessage('Please enter a Address'),
+    // check('address').notEmpty()
+    //     .withMessage('Please enter a Address'),
     check('username').notEmpty()
         .withMessage('Please enter a username'),
     check('username').matches(/^\w+([\._]?\w+)?@\w+(\.\w{2,3})(\.\w{2})?$/)
@@ -65,6 +65,8 @@ router.post('/signup',
     userController.userSignupPost);
 
 router.get('/cart', userController.userCartGet)
+
+router.get('/buyNowToCart/:productId', userController.buyNowToCart)
 
 router.get('/addToCart/:productId', userController.addToCartGet)
 
@@ -96,6 +98,59 @@ router.post('/mobileLogin',
 router.get('/otpLoginVerify', userController.otpLoginVerifyGet)
 
 router.post('/otpLoginVerify', userController.otpLoginVerifyPost)
+
+router.get('/profile', userController.profileGet)
+
+router.post('/addAddress',
+    check('newAddress').matches(/^((?!\n).)*$/)
+        .withMessage('Enter key not allowed'),
+    check('newAddress').notEmpty().withMessage('Please enter an address'),
+    userController.addAddress)
+
+router.post('/nameEdit',
+    check('newName').notEmpty().withMessage('Please enter a name'),
+    userController.nameEdit)
+
+router.post('/mobileEdit',
+    check('newMobile').matches(/[\d]{10}/)
+        .withMessage("Please enter a valid mobile number"),
+    check('newMobile').matches(/^[6-9][\d]{9}/)
+        .withMessage("Please enter a valid mobile number"),
+    userController.mobileEdit)
+
+router.post('/addressEdit/:id',
+    check('newAddress').notEmpty().withMessage('Please enter an address'),
+    userController.addressEdit)
+
+router.get('/addressDelete/:id', userController.addressDelete)
+
+router.post('/mobileChangeOtp', userController.mobileChangeOtp)
+
+router.get('/changePassword', userController.changePasswordGet)
+
+router.post('/changePassword',
+    check('password').matches(/[\w\d!@#$%^&*?]{8,}/)
+        .withMessage("Password must contain at least eight characters"),
+    check('password').matches(/[a-z]/)
+        .withMessage("Password must contain at least one lowercase letter"),
+    check('password').matches(/[A-Z]/)
+        .withMessage("Password must contain at least one uppercase letter"),
+    check('password').matches(/\d/)
+        .withMessage("Password must contain at least one number"),
+    check('password').matches(/[!@#$%^&*?]/)
+        .withMessage("Password must contain at least one special character"),
+    check('confirmPassword')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords do not match');
+            }
+            return true;
+        }),
+    userController.changePasswordPost)
+
+router.get('/passwordChangeOtp', userController.passwordChangeOtpGet)
+
+router.post('/passwordChangeOtp', userController.passwordChangeOtpPost)
 
 router.get('/logout', userController.userlogout);
 
