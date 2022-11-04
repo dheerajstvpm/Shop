d5d7f7505e1ddc9eaa1f9d542cec1e1966bea950
dheerajstvpm <dheerajstvpm@gmail.com>
2022-11-04 11:29:46 +0530

    Update offerModel.js

--- models/offerModel.js
+++ models/offerModel.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const offerSchema=new Schema({
     offerName:{type:String},
-    minOrder:{type:String},
-    discount:{type:String},
-    maxDiscount:{type:String}
+    minOrder:{type:Number},
+    discount:{type:Number},
+    maxDiscount:{type:Number}
 })
 
 
