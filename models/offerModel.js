2d1d03dec775d197153e6080a332d6cdaf5bbeba
dheerajstvpm <dheerajstvpm@gmail.com>
2022-11-09 12:39:53 +0530

    Update offerModel.js

--- models/offerModel.js
+++ models/offerModel.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema;
 
-const offerSchema=new Schema({
-    offerName:{type:String},
-    minOrder:{type:Number},
-    discount:{type:Number},
-    maxDiscount:{type:Number}
+const offerSchema = new Schema({
+    offerName: { type: String },
+    discount: { type: Number }
 })
 
 
-module.exports =mongoose.model('Offer', offerSchema)
+module.exports = mongoose.model('Offer', offerSchema)
