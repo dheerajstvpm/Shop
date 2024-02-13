483958f914b8167bdedefac7c4a353ddf8d049e4
Dheeraj S <dheerajstvpm@gmail.com>
2024-02-13 07:30:39 +0530

    for twilio error

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const fileUpload = require('express-fileupload')
 //env
 require('dotenv').config()
 // console.log(process.env)
-
+const client = require('twilio')(process.env.accountSid, process.env.authToken);
 
 
 
