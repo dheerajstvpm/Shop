4ffda49f39eae0c5d8a96e9c54c7cb8c00b6bd28
Dheeraj S <dheerajstvpm@gmail.com>
2024-02-13 07:55:17 +0530

    update node version

--- app.js
+++ app.js
@@ -107,5 +107,5 @@ app.use((req, res) => {
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+  console.log(`app listening on port ${process.env.PORT}`)
 });
