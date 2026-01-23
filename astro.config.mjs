cbd07d9626d547138550166660ce0b8a38bf6637
Dheeraj S <dheerajstvpm@gmail.com>
2026-01-23 20:24:24 +0530

    convert standard astro app without cloudflare dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,9 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 
 export default defineConfig({
     output: 'server',
-    adapter: cloudflare({
-        imageService: 'compile',
+    adapter: node({
+        mode: 'standalone',
     }),
-    vite: {
-        ssr: {
-            external: [
-                'node:crypto', 'crypto',
-                'node:fs', 'fs', 'node:fs/promises', 'fs/promises',
-                'node:path', 'path',
-                'node:process', 'process',
-                'node:stream', 'stream',
-                'node:util', 'util',
-                'node:events', 'events',
-                'node:buffer', 'buffer',
-                'node:url', 'url',
-                'node:http', 'http',
-                'node:https', 'https',
-                'node:zlib', 'zlib',
-                'node:net', 'net',
-                'node:tls', 'tls',
-                'node:os', 'os',
-                'node:child_process', 'child_process',
-                'node:timers', 'timers', 'node:timers/promises', 'timers/promises',
-                'node:dns', 'dns',
-                'async_hooks',
-                'assert'
-            ]
-        }
-    }
 });
