3662e0f3661f16267ba573d2c00f76a9f3394209
Dheeraj S <dheerajstvpm@gmail.com>
2026-01-23 19:47:26 +0530

    add external dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,28 @@ export default defineConfig({
     }),
     vite: {
         ssr: {
-            external: ['node:crypto', 'crypto']
+            external: [
+                'node:crypto', 'crypto',
+                'node:fs', 'fs', 'node:fs/promises', 'fs/promises',
+                'node:path', 'path',
+                'node:process', 'process',
+                'node:stream', 'stream',
+                'node:util', 'util',
+                'node:events', 'events',
+                'node:buffer', 'buffer',
+                'node:url', 'url',
+                'node:http', 'http',
+                'node:https', 'https',
+                'node:zlib', 'zlib',
+                'node:net', 'net',
+                'node:tls', 'tls',
+                'node:os', 'os',
+                'node:child_process', 'child_process',
+                'node:timers', 'timers',
+                'node:dns', 'dns',
+                'async_hooks',
+                'assert'
+            ]
         }
     }
 });
